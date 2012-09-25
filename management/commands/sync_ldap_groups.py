d4e744529bd91e6e86f1f537bb0ed70eb8a78af5
Roy Sindre Norangshol <roy.sindre@norangshol.no>
2012-09-25 11:13:48 +0200

    Pseudo wiring of the management command, work in progress

--- management/commands/sync_ldap_groups.py
+++ management/commands/sync_ldap_groups.py
@@ -16,9 +16,38 @@ Process one user at the time and do:
 * Remove groups that the user isn't associated with anylonger in LDAP
 """
 from django.core.management import BaseCommand
+from rt_ldap_sync.models import RtGroup, RtUser, RtGroupMember, USER_DEFINED
 
 class Command(BaseCommand):
     help = 'Synchronizes the LDAP groups and membership to RequestTracker'
 
     def handle(self, *args, **options):
+        self.ldap_username_list =['norangsh', 'sigurf']
+        self.ldap_user_group_map = {
+            'norangsh': ['xdotkom'],
+            'sigurf': ['dotkom']
+        }
+
         self.stdout.write("This management command doesn't exactly nothing atm!")
+        groups_to_create = RtGroup.objects.find_groups_not_listed(['dotkom', 'xdotkom'])
+        if groups_to_create:
+            for group in groups_to_create:
+                RtGroup.objects.create(name=group, typed=USER_DEFINED)
+
+        ldap_username_list = ['norangsh', 'sigurf']
+
+        rt_users = RtUser.objects.filter(name__in=ldap_username_list)
+        for user in rt_users:
+            user_groups = self._get_groups_from_ldap_user(user)
+
+            for group in RtGroupMember.objects.extra_ldap_groups(user, user_groups):
+                RtGroupMember.objects.create(group=RtGroup.objects.get(name=group), member=user)
+
+            RtGroupMember.objects.filter(group__in=RtGroupMember.objects.extra_rt_groups(user, user_groups), member=user).delete()
+
+
+    def _get_groups_from_ldap_user(self, ldap_username):
+        try:
+            return self.ldap_user_group_map[ldap_username]
+        except KeyError:
+            return []
