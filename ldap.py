64d3c296644162fbb8e759ac117a72d196138954
Roy Sindre Norangshol <roy.sindre@norangshol.no>
2012-09-30 03:22:33 +0200

    Working on unit tests and implementation of ldap controller

--- ldap.py
+++ ldap.py
@@ -1,35 +1,111 @@
 """Tiny LDAP Wrapper module"""
+import simpleldap
 
-class LDAP(object):
+class LdapController(object):
+    def __init__(self, ldap_impl=None):
+        self._connection = None
+        self._search_base = None
+        self._encryption = None
+        self._protocol = None
 
-        def connected(self, meta):
-            """Connect to LDAP server"""
-            pass
+        self._ldap_impl = ldap_impl if ldap_impl else simpleldap
 
-        def is_connected(self):
-            """are we connected to LDAP server?
 
-            :returns true if we are.
-            """
-            pass
+    def connect(self, hostname, port, search_base='', in_protocol='ldaps'):
+        """Connect to LDAP server
 
-        def get_users(self, search_options=None):
-            """Get users from LDAP
+        """
+        if self._connection:
+            return self._connection
 
-            :search_options may contain search parameters as:
+        if not hostname:
+            raise ValueError('Requires a hostname to connect to')
+        if not port or (port and (port <= 0 or port > 65535)):
+            raise ValueError('Requires a valid port number.')
 
-            BaseDN, isUserSubTree?, ObjectClass required, UserFilter
-            UserIDAttribute, RealNameAttribute, EmailAttribute
-            """
-            pass
+        if port == 636:
+            self._protocol = 'ldaps'
+        else:
+            self._protocol = in_protocol
 
-        def get_groups(self, username, search_options=None):
-            """Get group given user name
+        self.hostname = hostname
+        self.port = port
+        self._search_base = search_base
 
-            :username groupmembership for given username
-            :search_options search meta for group fetching:
+        if self._protocol == 'ldapi':
+            self._encryption = None
+        elif self._protocol == 'ldaps':
+            self._encryption = 'ssl'
+        else:
+            self._encryption = 'tls'
 
-            GroupTye (Static|Dynamic), BaseDN, Group Subtree,
-            ObjectClass, GroupIDAttribute, GroupMemberAttribute,
-            GroupMemeberFormat"""
-            pass
+
+
+        self._connection = self._ldap_impl.Connection(self.hostname, self.port, '', '', self._encryption )
+        return self._connection
+
+
+    def close(self):
+        if self._connection:
+            self._connection.close()
+        self._connection = None
+
+    def is_connected(self):
+        """are we connected to LDAP server?
+
+        :returns true if we are.
+        """
+        return self._connection is not None
+
+    def get_connection(self):
+        return self._connection
+
+
+    def _get_search_results(self, filter, base_dn, attributes):
+        return self.get_connection().search(filter, base_dn, attributes)
+
+    def get_users(self, search_options=None):
+        """Get users from LDAP
+
+        :search_options may contain search parameters as:
+
+        BaseDN, isUserSubTree?, ObjectClass required, UserFilter
+        UserIDAttribute, RealNameAttribute, EmailAttribute
+        """
+
+        #base_dn_users = 'ou=people'
+        base_dn_users = self._search_base
+        #filter_object_class = '(objectClass=inetOrgPerson)'
+        filter_object_class = '(objectClass=posixAccount)'
+        attribute_user_id = 'uid'
+        attribute_real_name = 'displayName'
+        attribute_email = 'mail'
+
+        if self.is_connected():
+            return self._get_search_results(filter_object_class, base_dn_users, None)
+        else:
+            raise simpleldap.ConnectionException('You need to be connected')
+
+    def get_groups(self, username, search_options=None):
+        """Get group given user name
+
+        :username groupmembership for given username
+        :search_options search meta for group fetching:
+
+        GroupTye (Static|Dynamic), BaseDN, Group Subtree,
+        ObjectClass, GroupIDAttribute, GroupMemberAttribute,
+        GroupMemeberFormat"""
+
+        is_group_type_static = True
+        base_dn_group = 'ou=groups'
+        filter_object_class = 'posixGroup'
+        attribute_group_id = 'cn'
+        attribute_member = 'memberUid'
+
+        if self.is_connected():
+            return self.get_connection().search(filter_object_class, base_dn_group,
+            [attribute_group_id, attribute_member])
+        else:
+            raise simpleldap.ConnectionException('You need to be connected')
+
+
--- tests.py
+++ tests.py
@@ -1,6 +1,12 @@
 """RT LDAP Sync tests"""
+import unittest
+import mock
+import simpleldap
+
 from django.db import IntegrityError
 from django.test import TestCase
+
+from rt_ldap_sync.ldap import LdapController
 from rt_ldap_sync.models import RtGroup, USER_DEFINED, RT_QUEUE_ROLE, RtUser, RtGroupMember, USER_ID_DEFAULT
 
 
@@ -120,23 +126,103 @@ class GroupMember(TestCase):
 
         self.assertEqual(['trollkom', 'xdotkom'], sorted(extra_ldap + extra_rt))
 
-class LDAP(TestCase):
 
-    def test_connect_success(self):
-        self.fail('Implement test')
+#class Troll(object):
+#    class Connection(object):
+#        def __init__(self, hostname, port, bind_dn, bind_pw, encryption):
+#            pass
 
-    def test_connect_failes(self):
-        self.fail('Implement test')
+class LDAPTestCase(unittest.TestCase):
 
-    def test_is_connected(self):
-        self.fail('Implement test')
 
-    def test_raise_exception_when_doing_command_and_not_connected(self):
-        self.fail('Implement test')
+    def setUp(self):
+        mock_ldap = mock.MagicMock(simpleldap)
+        self.module = LdapController(mock_ldap)
 
-    def test_get_users(self):
-        self.fail('Implement test')
+    def _mock_valid_connection(self):
+        connection_mock = mock.MagicMock(name='LdapController._connection')
+        connection_mock.return_value = True
+        self.module.Connection = connection_mock
+        #setattr(self.module, '_connection', connection_mock)
 
-    def test_get_groups(self):
-        self.fail('Implement test')
+    def test_connect_fail_no_hostname(self):
+        self.assertRaises(ValueError, lambda: self.module.connect(None, 412))
 
+    def test_connect_hostname_set(self):
+        self.module.connect('a_hostname', 666, None, None)
+        self.assertEquals('a_hostname', getattr(self.module, 'hostname'))
+
+    def test_connect_fail_no_port(self):
+        self.assertRaises(ValueError, lambda: self.module.connect('foo', None))
+        self.assertRaises(ValueError, lambda: self.module.connect('foo', 0))
+
+    def test_connect_fail_port_below_1(self):
+        self.assertRaises(ValueError, lambda: self.module.connect('foo', -1))
+
+    def test_connect_fail_port_above_65535(self):
+        self.assertRaises(ValueError, lambda: self.module.connect('foo', 65536))
+
+    def test__mock_valid_connection(self):
+        self._mock_valid_connection()
+        self.assertFalse(getattr(self.module, '_connection'))
+        self.assertFalse(self.module.is_connected())
+        self.assertTrue(self.module.connect('foo', 1))
+        self.assertTrue(getattr(self.module, '_connection'))
+        self.assertTrue(self.module.is_connected())
+
+        self.module.close()
+        self.assertFalse(getattr(self.module, '_connection'))
+        self.assertFalse(self.module.is_connected())
+
+    def test_connect_min_valid_port(self):
+        self._mock_valid_connection()
+        self.assertTrue(self.module.connect('foo', 1))
+
+    def test_connect_max_valid_port(self):
+        self._mock_valid_connection()
+
+        self.assertTrue(self.module.connect('foo', 65535))
+
+
+    def test_if_protocol_is_ssl_on_ssl_port(self):
+        self.module.connect('foo', 636, None, 'ldap')
+        self.assertEquals('ldaps', getattr(self.module, '_protocol'))
+
+    def test_if_protocol_ldap(self):
+        self.module.connect('foo', 2323, None, 'ldap')
+        self.assertEquals('ldap', getattr(self.module, '_protocol'))
+
+    def test_if_protocol_ldaps(self):
+        self.module.connect('foo', 2323, None, 'ldaps')
+        self.assertEquals('ldaps', getattr(self.module, '_protocol'))
+
+    def test_get_users_without_connection(self):
+        self.assertRaises(simpleldap.ConnectionException, lambda: self.module.get_users())
+
+    def test_get_groups_without_connection(self):
+        self.assertRaises(simpleldap.ConnectionException, lambda: self.module.get_groups('foo'))
+
+    def test_get_users_single_row(self):
+        self._mock_valid_connection()
+        self.assertTrue(self.module.connect('foo', 1))
+        self.module._get_search_results = mock.MagicMock(name='ldap._get_search_results')
+        self.module._get_search_results.return_value = [{'cn': ['tadaa'], 'objectclass': ['account', 'posixAccount'], 'loginshell': ['/bin/bash'], 'uidnumber': ['1000'], 'gidnumber': ['1000'], 'gecos': ['Rockj'], 'homedirectory': ['/home/rockj'], 'uid': ['tadaa']}]
+
+        #[{'cn': ['tadaa'], 'objectclass': ['account', 'posixAccount'], 'loginshell': ['/bin/bash'], 'uidnumber': ['1000'], 'gidnumber': ['1000'], 'gecos': ['Rockj'], 'homedirectory': ['/home/rockj'], 'uid': ['tadaa']}]
+        results = self.module.get_users()
+        self.assertEquals(1, len(results))
+
+        self.assertEquals('tadaa', results[0]['cn'][0])
+        self.assertTrue(results[0]['uidnumber'])
+        self.assertEquals('1000', results[0]['uidnumber'][0])
+
+    def test_get_users_multiple_rows(self):
+        self._mock_valid_connection()
+        self.assertTrue(self.module.connect('foo', 1))
+        self.module._get_search_results = mock.MagicMock(name='ldap._get_search_results')
+        self.module._get_search_results = mock.MagicMock(name='ldap._get_search_results')
+        self.module._get_search_results.return_value = [{'cn': ['tadaa'], 'objectclass': ['account', 'posixAccount'], 'loginshell': ['/bin/bash'], 'uidnumber': ['1000'], 'gidnumber': ['1000'], 'gecos': ['Rockj'], 'homedirectory': ['/home/rockj'], 'uid': ['tadaa']}, {'cn': ['jadda'], 'objectclass': ['account', 'posixAccount'], 'loginshell': ['/bin/bash'], 'uidnumber': ['1001'], 'gidnumber': ['1001'], 'gecos': ['Rasasd'], 'homedirectory': ['/home/sdaf'], 'uid': ['jadda']}]
+
+        results = self.module.get_users()
+
+        self.assertEquals(2, len(results))
